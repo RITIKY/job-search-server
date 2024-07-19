0ca06b0c3649770aed6de96391f1b1e51a206445
Ritik Yadav <ritikyadav0178@gmail.com>
2024-07-20 01:22:34 +0530

    change origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 // CORS configuration
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
